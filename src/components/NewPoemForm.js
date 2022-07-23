f1dbce955be6b75457db98b7cf4518007e895e74
Koima-Sam <sam17koima@gmail.com>
2022-07-23 22:14:37 +0300

    Done

--- src/components/NewPoemForm.js
+++ src/components/NewPoemForm.js
@@ -1,42 +1,60 @@
 import React, { useState } from "react";
 
 function NewPoemForm() {
-  const[formData,setFormData] = useState({
+  const [formData, setFormData] = useState({
     title: "",
     content: "",
     author: "",
-  })
+  });
   //declare function to hadle form submit
-  function handleFormSubmit(event){
-    event.preventDefault()
+  function handleFormSubmit(event) {
+    event.preventDefault();
     //Fetch the json api and POST
-    fetch("http://localhost:8004/poems",{
-      method:"POST",
-      headers:{
-        'Content-Type':'application/json'
-      },
-      body:JSON.stringify(formData)
-    })
-    .then(response => response.json)
-    .then(data => {
-      // updatePoemRecords()
-      setFormData({...formData, title:"",author:"",content:"" })
-    })
-
+    if (formData.title === "" || formData.content === "" || formData === "") {
+      alert("Fill all inputs");
+    } else {
+      fetch("http://localhost:8004/poems", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+        .then((response) => response.json)
+        .then((data) => {
+          // updatePoemRecords()
+          setFormData({ ...formData, title: "", author: "", content: "" });
+        });
+    }
   }
   //Capture changes within our form and update states
- function handleChange(event){
-  setFormData({...formData, [event.target.name]:event.target.value})
- }
-//  console.log(formData.title)
-//  console.log(formData.author)
-
+  function handleChange(event) {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  }
+  //  console.log(formData.title)
+  //  console.log(formData.author)
 
   return (
-    <form className="new-poem-form"  onSubmit={handleFormSubmit}>
-      <input placeholder="Title" name ='title' value={formData.title} onChange={handleChange}/>
-      <input placeholder="Author" name ='author' value={formData.author} onChange ={handleChange}/>
-      <textarea placeholder="Write your masterpiece here..."  name ='content' rows={10} value={formData.content} onChange={handleChange}/>
+    <form className="new-poem-form" onSubmit={handleFormSubmit}>
+      <input
+        placeholder="Title"
+        name="title"
+        value={formData.title}
+        onChange={handleChange}
+      />
+      <input
+        placeholder="Author"
+        name="author"
+        value={formData.author}
+        onChange={handleChange}
+      />
+      <textarea
+        placeholder="Write your masterpiece here..."
+        name="content"
+        rows={10}
+        value={formData.content}
+        onChange={handleChange}
+      />
       <input type="submit" value="Share your masterpiece" />
     </form>
   );
--- src/components/PoemsContainer.js
+++ src/components/PoemsContainer.js
@@ -8,24 +8,24 @@ function PoemsContainer() {
     fetch("http://localhost:8004/poems")
       .then((response) => response.json())
       .then((data) => setPoems(data));
-  },[poems]);
+  }, [poems]);
 
-  const poemItem = poems.map((poem)=>{
-    return <Poem
-    key={poem.id}
-    id={poem.id}
-    title = {poem.title}
-    content = {poem.content}
-    author = {poem.author}
-    />
-  })
-  
+  const poemItem = poems.map((poem) => {
+    return (
+      <Poem
+        key={poem.id}
+        id={poem.id}
+        title={poem.title}
+        content={poem.content}
+        author={poem.author}
+      />
+    );
+  });
 
   return (
     <div className="poems-container">
       {/* render a list of <Poem> components in here */}
       {poemItem}
-      
     </div>
   );
 }
